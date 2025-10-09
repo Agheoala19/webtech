a0b5acc2a1be9e2c18675a4bcc87a1ab2eaf1a80
Alexandru Gheoala <162614973+Agheoala19@users.noreply.github.com>
2025-10-09 17:50:36 +0300

    Update hello.js

--- seminar2/hello.js
+++ seminar2/hello.js
@@ -1,1 +1,14 @@
+const { join } = require("path")
 
+let sayHello = (name) => {
+    return `Hello, ${name}!`
+}
+
+console.log(sayHello(process.argv[0]))
+console.log(sayHello(process.argv[1]))
+console.log(sayHello(process.argv[2]))
+
+let words = ["Hello", " ", "world!"]
+let concatenation = (string) => string.join('')
+
+console.log(concatenation(words))
