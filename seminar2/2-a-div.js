28209c770a3f2d8d1deb7d4c086c35ded9dd6706
Alexandru Gheoala <162614973+Agheoala19@users.noreply.github.com>
2025-10-09 17:51:02 +0300

    Update 2-a-div.js

--- seminar2/2-a-div.js
+++ seminar2/2-a-div.js
@@ -1,1 +1,27 @@
+function checkDivisible(n, divisor) {
+    if (n % divisor) {
+        return false
+    } else {
+        return true
+    }
+}
 
+console.log(checkDivisible(10, 2))
+console.log(checkDivisible(10, 3))
+
+function noOfDifferentCharacters(str1, str2) {
+    if (str1.length !== str2.length) {
+        return -1
+    }
+
+    let differentCharacters = 0
+    for (let i = 0; i < str1.length; i++) {
+        if (str1[i] !== str2[i]) {
+            differentCharacters++
+        }
+    }
+
+    return differentCharacters
+}
+
+console.log(noOfDifferentCharacters("mama", "tata"))
