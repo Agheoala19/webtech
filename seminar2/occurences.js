56637203c7315f323e78ac64d6a251b563a95e6d
Alexandru Gheoala <162614973+Agheoala19@users.noreply.github.com>
2025-10-09 17:51:49 +0300

    Update occurences.js

--- seminar2/occurences.js
+++ seminar2/occurences.js
@@ -1,1 +1,21 @@
+function occurences(text, character) {
+    // let count = 0
+    // for (var i = 0; i < text.length; i++) {
+    //     if (text.charAt(i) === character) {
+    //         count++
+    //     }
+    // }
+    // return count
+    return text.split(character).length - 1
+}
 
+let occurences2 = (text, character) => text.split(character).length - 1
+
+console.log(occurences("sample text", "e"))
+console.log(occurences2("sample text", "e"))
+
+function listToArray(...list) {
+    return list;
+}
+
+console.log(listToArray(1, 5, 9, 12))
