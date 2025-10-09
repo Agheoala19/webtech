fa153ff6bee660d9eda3cac9be05ce329f615a9a
Alexandru Gheoala <162614973+Agheoala19@users.noreply.github.com>
2025-10-09 17:52:34 +0300

    Update 5-a-prime.js

--- seminar2/5-a-prime.js
+++ seminar2/5-a-prime.js
@@ -1,1 +1,36 @@
+const checkPrime = (n) => {
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (!(n % i)) {
+            return false
+        }
+    }
+    return true
+}
 
+if (process.argv.length <= 2) {
+    console.log('not enough parameters')
+} else {
+    console.log(checkPrime(parseInt(process.argv[2])))
+}
+
+const fibonacci = (n) => {
+    if (n < 0) return -1;
+    if (n === 0) return 0;
+    if (n === 1) return 1;
+
+    let a = 0, b = 1, c;
+    for (let i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
+const n = parseInt(process.argv[2]);
+
+if (isNaN(n)) {
+    console.log("Enter a valid number!");
+} else {
+    console.log(`The element of index ${n} in Fibonacci is: ${fibonacci(n)}`);
+}
